8a8f37ac0de3826d573bf08c8575adbba5ed2d96
Giulia Vieira <giuliamsgvieira@gmail.com>
2024-10-22 17:48:28 -0300

    FIX: test file names

--- test.py
+++ test.py
@@ -6,15 +6,15 @@ test_cases_dir = "test_cases"
 
 # List of test cases with input and output file names
 test_cases = [
-    (1, os.path.join(test_cases_dir, "input_1.txt"), os.path.join(test_cases_dir, "output_1.txt")),
-    (2, os.path.join(test_cases_dir, "input_2.txt"), os.path.join(test_cases_dir, "output_2.txt")),
-    (3, os.path.join(test_cases_dir, "input_3.txt"), os.path.join(test_cases_dir, "output_3.txt")),
-    (4, os.path.join(test_cases_dir, "input_4.txt"), os.path.join(test_cases_dir, "output_4.txt")),
-    (5, os.path.join(test_cases_dir, "input_5.txt"), os.path.join(test_cases_dir, "output_5.txt")),
-    (6, os.path.join(test_cases_dir, "input_6.txt"), os.path.join(test_cases_dir, "output_6.txt")),
-    (7, os.path.join(test_cases_dir, "input_7.txt"), os.path.join(test_cases_dir, "output_7.txt")),
-    (8, os.path.join(test_cases_dir, "input_8.txt"), os.path.join(test_cases_dir, "output_8.txt")),
-    (9, os.path.join(test_cases_dir, "input_9.txt"), os.path.join(test_cases_dir, "output_9.txt")),
+    (1, os.path.join(test_cases_dir, "input_01.txt"), os.path.join(test_cases_dir, "output_01.txt")),
+    (2, os.path.join(test_cases_dir, "input_02.txt"), os.path.join(test_cases_dir, "output_02.txt")),
+    (3, os.path.join(test_cases_dir, "input_03.txt"), os.path.join(test_cases_dir, "output_03.txt")),
+    (4, os.path.join(test_cases_dir, "input_04.txt"), os.path.join(test_cases_dir, "output_04.txt")),
+    (5, os.path.join(test_cases_dir, "input_05.txt"), os.path.join(test_cases_dir, "output_05.txt")),
+    (6, os.path.join(test_cases_dir, "input_06.txt"), os.path.join(test_cases_dir, "output_06.txt")),
+    (7, os.path.join(test_cases_dir, "input_07.txt"), os.path.join(test_cases_dir, "output_07.txt")),
+    (8, os.path.join(test_cases_dir, "input_08.txt"), os.path.join(test_cases_dir, "output_08.txt")),
+    (9, os.path.join(test_cases_dir, "input_09.txt"), os.path.join(test_cases_dir, "output_09.txt")),
     (10, os.path.join(test_cases_dir, "input_10.txt"), os.path.join(test_cases_dir, "output_10.txt")),
 ]
 
